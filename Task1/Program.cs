33b66f9d20b0face11a334981fdb40d53e2c8f3a
Valentin.Kostin <frezer.2021@mail.ru>
2023-02-07 08:36:28 +0300

    всё работает

---
+++ Task1/Program.cs
@@ -0,0 +1,47 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+int PositiveNumbers(int[] intArray)
+{
+    int count = 0;
+    for (int i = 0; i < intArray.Length; i++)
+    {
+        if (intArray[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] GetArrayFromConsole(int length)
+{
+    int[] arrayNum = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"Введите {i+1} число: ");
+        arrayNum[i] = int.Parse(Console.ReadLine());
+    }
+    return arrayNum;
+}
+
+int GetNum(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+// Запросим сколько чисел вводить 
+int lengthNum = GetNum("Введите количество чисел: ");
+
+// Ввод самих чисел
+int[] numArray = GetArrayFromConsole(lengthNum);
+
+// Считаем сколько положительных чисел
+int countPositiv = PositiveNumbers(numArray);
+
+Console.Write($"[{String.Join(" ,", numArray)}] -> {countPositiv}");
+
---
+++ Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задача 43.
+Напишите программу, которая найдёт точку пересечения двух прямых
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+k1 * x + b1 = k2 * x + b2
+x(k1 - k2) = b2 - b1
+x = (b2 - b1) / (k1 - k2)*/
+
+void GetPointOfIntersection(int b1, int k1, int b2, int k2)
+{
+    if (k1 - k2 != 0)
+    {
+        float x = (float)(b2 - b1) / (float)(k1 - k2);
+        float y = k1 * x + b1;
+        Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+    }
+    else
+    {
+        Console.WriteLine("прямые не пересекаются или совпадают");
+    }
+}
+
+int GetNum(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+// Запросим числа 
+int b1 = GetNum("Введите число b1: ");
+int k1 = GetNum("Введите число k1: ");
+int b2 = GetNum("Введите число b2: ");
+int k2 = GetNum("Введите число k2: ");
+
+GetPointOfIntersection(b1, k1, b2, k2);
